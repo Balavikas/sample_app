26fbfb0f8dcb1980f9c6a0a0d50699b6892031c4
Balavikas <balavikass999@gmail.com>
2025-06-29 15:18:13 -0500

    make a basic user model (including a secure passwords)

---
+++ test/models/user_test.rb
@@ -0,0 +1,76 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = User.new(name: 'balavikas', email: 'balavikass999@gmail.com', password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email is a valid format" do
+    valid_email = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+
+    valid_email.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} is a valid email"
+    end
+  end
+
+  test "email is not a valid format" do
+    invalid_emails = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+
+    invalid_emails.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "#{invalid_email.inspect} is invalid email"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should converted to downcase" do
+    mixed_case_email = 'FooBar@examPLe.Com'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password must not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password is not valid when less than 6 charcaters" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
