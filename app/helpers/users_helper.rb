8054239580f681155e732737dc64263a01cf5667
Balavikas <balavikass999@gmail.com>
2025-07-08 21:16:19 -0500

    add avatar method to display user initials

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
+
+    def avatar(user)
+        full_name = user.name
+        avatar_url = "https://avatar.iran.liara.run/username?username=#{full_name}"
+        image_tag(avatar_url, alt: user.name, class: "garavatar", width: 150, height: 150)
+    end
 end
