2f5ad3af28dee6953f3dcaf96b5155e0ed8c2692
Balavikas <balavikass999@gmail.com>
2025-07-09 13:54:10 -0500

    Create a flash message when user signup and add a valid signup scenario for integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # render a correct templete
+      flash[:success] = "welcome to the sample app"
+      redirect_to @user
     else
-      render 'new'
+      render "new"
     end
   end
 
