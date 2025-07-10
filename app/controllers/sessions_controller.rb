058d1d92cc1da8408d782cc3c5391608e7e1fc0b
Balavikas <balavikass999@gmail.com>
2025-07-10 09:59:55 -0500

    add helper methods in session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      # some templete
+      log_user user
+      redirect_to user
     else
       flash[:danger] = "Invalid Email/Password combinations"
       render "new"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def log_user(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= user.find_by(id: session[:user_id])
+    end
+  end
 end
