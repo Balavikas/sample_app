f2562b322935ccbea8ac9a7eeda9dcc2697f73ef
Balavikas <balavikass999@gmail.com>
2025-07-10 14:25:12 -0500

    uninstall jquery-rails and followed rails 7 michael hart book

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,14 @@
+// Menu manipulation
+// Add toggle listeners to listen for clicks.
+document.addEventListener("turbo:load", function () {
+  let account = document.querySelector("#account");
+  if (account) {
+    account.addEventListener("click", function (event) {
+      event.preventDefault();
+      let menu = document.querySelector("#dropdown-menu");
+      if (menu) {
+        menu.classList.toggle("active");
+      }
+    });
+  }
+});
