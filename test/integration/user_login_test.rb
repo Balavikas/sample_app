3a3176437f8e320c00cee4cb0ff809ead27991e3
Balavikas <balavikass999@gmail.com>
2025-07-11 15:16:26 -0500

    add a test scenario for a valid login

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UserLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:uday)
+  end
+
   test "invalid login" do
     get login_path
     assert_template "sessions/new"
@@ -9,5 +13,16 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert_not flash.empty?
+  end
+
+  test "valid login" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: { email: @user.email, password: "New1user!" } }
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
